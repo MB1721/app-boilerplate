09a607162e1e8d10d32a02706040c752c13d18f4
mb1721 <masonbennett1721@gmail.com>
2024-03-21 00:04:28 -0400

    Remove log

--- app/assets/scripts/package/compile.js
+++ app/assets/scripts/package/compile.js
@@ -8,10 +8,7 @@ async function compileViews() {
   const build = process.argv[2];
   const views = await getViews();
   try {
-    for (const view of views) {
-      console.log(`Building ${view}`);
-      await execProm(`cd "${view}" && npm run ${build}`);
-    }
+    for (const view of views) await execProm(`cd "${view}" && npm run ${build}`);
   } catch (err) {
     console.error(err);
   }
